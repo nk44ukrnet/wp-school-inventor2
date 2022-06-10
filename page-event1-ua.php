ef177a67201861223b3c368a4d0a9847018151a2
nk44ukrnet <56731417+nk44ukrnet@users.noreply.github.com>
2022-06-10 17:18:49 +0300

    video to event page added

--- page-event1-ua.php
+++ page-event1-ua.php
@@ -35,6 +35,10 @@ while (have_posts()) {
 			<!--data-bs-toggle="modal" data-bs-target="#contactEventPopup"-->
         </div>
     </section>
+    
+    <section class="event-video">
+        <iframe src="https://player.vimeo.com/video/719114215?h=0dbaeabba5&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="1920" height="1080" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="Космічна подорож.mp4"></iframe>
+    </section>
 
     <div class="event_descreption">
         <h2>
