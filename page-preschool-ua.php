72be246621d787975e25441a85d3e5b86daeceda
nk44ukrnet <56731417+nk44ukrnet@users.noreply.github.com>
2023-05-24 12:07:23 +0300

    iframe changed

--- page-preschool-ua.php
+++ page-preschool-ua.php
@@ -102,7 +102,7 @@ while ( have_posts() ) {
     </div>
 
     <div class="video-general inner_wrapper padding-mobile">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/ZZL1uIBLWp0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/QH2DCG7Q630" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
 
     <section class="preschool_develop">
